37014a9294a7edcbf99bb177aa4a001e6a47e90f
seryga-afk <seryga_94.94@mail.ru>
2022-02-24 21:32:07 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let stack = [];
+  str = str.split('');
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].indexOf() === 0) {
+      stack.push(str[i])
+      return
+    }
+    let last = bracketsConfig.find(bracket=> stack[stack.length - 1] === bracket[0])
+    last = last && last[1]
+  
+    if (str[i] !== last) {
+      stack.push(str[i])
+    }else{
+      stack.pop()
+    }
+  }
+
+  return stack.length === 0;
+};
